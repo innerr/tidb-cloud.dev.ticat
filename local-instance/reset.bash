ebff7fe52f8d8445ffce8c75375f8d9cd931d7b0
Liu Cong <innerr@gmail.com>
2023-06-02 20:32:53 +0800

    fix import in script
    Signed-off-by: Liu Cong <innerr@gmail.com>

--- local-instance/reset.bash
+++ local-instance/reset.bash
@@ -1,5 +1,5 @@
 set -euo pipefail
-. "`cd $(dirname ${BASH_SOURCE[0]}) && pwd`/../../../helper/helper.bash"
+. "`cd $(dirname ${BASH_SOURCE[0]}) && pwd`/../helper/helper.bash"
 env=`cat "${1}/env"`
 
 repo_dir=`must_env_val "${env}" 'tidb-cloud.dev.tidb-mgmt.repo-dir'`
