e1a91bb28371e13165a3833df0bc48a40a30ddcb
Liu Cong <innerr@gmail.com>
2023-06-02 20:31:13 +0800

    add cmd `lc.reset`
    Signed-off-by: Liu Cong <innerr@gmail.com>

---
+++ local-instance/reset.bash
@@ -0,0 +1,10 @@
+set -euo pipefail
+. "`cd $(dirname ${BASH_SOURCE[0]}) && pwd`/../../../helper/helper.bash"
+env=`cat "${1}/env"`
+
+repo_dir=`must_env_val "${env}" 'tidb-cloud.dev.tidb-mgmt.repo-dir'`
+data_dir="${repo_dir}/.ticat/data"
+
+echo "[:-] rm -rf ${data_dir}"
+rm -rf "${data_dir}"
+echo '[:)] done'
